e1e38c6991f71ceddc2d1ca009af488e6aca5f98
Leandro Belge da Cunha <leandrobelge@gmail.com>
2016-06-30 14:12:31 -0300

    Iniciando o projeto

---
+++ GerenciadorArquivoCNAB240/GerenciadorArquivoCNAB240/Domains/ClsHeaderArquivo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorArquivoCNAB240.Domains
+{
+    public class ClsHeaderArquivo
+    {
+        public string banco { get; set; }
+        public string lote { get; set; }
+        public string registro { get; set; }
+        public string CNAB { get; set; }
+        public TipoPessoa TipoInscricao { get; set; }
+    }
+}
---
+++ GerenciadorArquivoCNAB240/GerenciadorArquivoCNAB240/Domains/ClsTipoPessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorArquivoCNAB240.Domains
+{
+    public static class TipoPessoa
+    {
+        enum Tipo { CPF, CNPJ };
+
+        public static Tipo tipoPessoa { get; set; }
+        
+    }
+}
